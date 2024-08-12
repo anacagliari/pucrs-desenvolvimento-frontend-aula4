a691b5350f6ed244f96c19708b6275855dfee231
Ana Cagliari <carolinecagliari@gmail.com>
2024-08-12 10:04:44 -0300

    pucrs-desenvolvimento-frontend-aula4

--- src/App.js
+++ src/App.js
@@ -1,25 +1,33 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from 'react';
 
-function App() {
+/* explicação melhor Aula 4 Parte 2 - a partir do 28:00*/
+import { FaStar } from 'react-icons/fa';
+
+
+function StarRating({ nStarts }) {
+  const [rate, setRate] = useState(0);
+  const starList = Array.from({ length: nStarts }, (_, k) => k + 1)
+
+  function evaluate(star) {
+    setRate(star)
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <>
+      {
+        starList.map(
+          (n) => <FaStar
+            onClick={_ => evaluate(n)}
+            color={n <= rate ? 'gold' : 'lightgray'}></FaStar>
+        )
+      }
+      <p>O rate atual é {rate}</p>
+    </>
+  )
 }
 
-export default App;
+export default function App() {
+  return (
+    <StarRating nStarts={5} />
+  );
+}
